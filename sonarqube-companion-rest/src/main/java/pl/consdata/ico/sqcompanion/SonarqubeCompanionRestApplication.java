36748b08a7c7b83a1b9795ed30047d0c8cbda9f2
mnowak <mnowak@consdata.pl>
2019-06-05 08:03:53 +0200

    Add Mongo repository

--- sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/SonarqubeCompanionRestApplication.java
+++ sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/SonarqubeCompanionRestApplication.java
@@ -3,6 +3,7 @@ package pl.consdata.ico.sqcompanion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @EnableCaching
+@EnableMongoRepositories
 public class SonarqubeCompanionRestApplication {
 
     public static void main(String[] args) {
